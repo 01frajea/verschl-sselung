b5fd26e56e80ffd98b879a0dbf7ec9570c3835e7
01frajea <01frajea@rgtfo-me.it>
2018-03-02 12:45:33 +0100

    Programm eingefügt

---
+++ verschlüsseln/verschlüsseln/Form1.Designer.cs
@@ -0,0 +1,162 @@
+﻿namespace verschlüsseln
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Erforderliche Designervariable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Verwendete Ressourcen bereinigen.
+        /// </summary>
+        /// <param name="disposing">True, wenn verwaltete Ressourcen gelöscht werden sollen; andernfalls False.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Vom Windows Form-Designer generierter Code
+
+        /// <summary>
+        /// Erforderliche Methode für die Designerunterstützung.
+        /// Der Inhalt der Methode darf nicht mit dem Code-Editor geändert werden.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.entschlüsseltlbl = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.clear = new System.Windows.Forms.Button();
+            this.verzahltrack = new System.Windows.Forms.TrackBar();
+            ((System.ComponentModel.ISupportInitialize)(this.verzahltrack)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // entschlüsseltlbl
+            // 
+            this.entschlüsseltlbl.Location = new System.Drawing.Point(51, 135);
+            this.entschlüsseltlbl.Name = "entschlüsseltlbl";
+            this.entschlüsseltlbl.Size = new System.Drawing.Size(117, 20);
+            this.entschlüsseltlbl.TabIndex = 0;
+            this.entschlüsseltlbl.TextChanged += new System.EventHandler(this.entschlüsseld_TextChanged);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(42, 96);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(116, 13);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "text zum verschlüsseln:";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(332, 142);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(0, 13);
+            this.label2.TabIndex = 6;
+            this.label2.Click += new System.EventHandler(this.label2_Click);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(287, 96);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(100, 13);
+            this.label3.TabIndex = 7;
+            this.label3.Text = "verschlüsselter text:";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(12, 9);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(273, 25);
+            this.label4.TabIndex = 8;
+            this.label4.Text = "Verschlüsselungprogramm:";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.Location = new System.Drawing.Point(200, 130);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(45, 24);
+            this.label5.TabIndex = 9;
+            this.label5.Text = "---->";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(12, 353);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(108, 13);
+            this.label6.TabIndex = 10;
+            this.label6.Text = "Franzelin Patrick 3bel";
+            // 
+            // clear
+            // 
+            this.clear.Location = new System.Drawing.Point(191, 236);
+            this.clear.Name = "clear";
+            this.clear.Size = new System.Drawing.Size(75, 23);
+            this.clear.TabIndex = 11;
+            this.clear.Text = "clear";
+            this.clear.UseVisualStyleBackColor = true;
+            this.clear.Click += new System.EventHandler(this.clear_Click);
+            // 
+            // verzahltrack
+            // 
+            this.verzahltrack.Location = new System.Drawing.Point(51, 185);
+            this.verzahltrack.Name = "verzahltrack";
+            this.verzahltrack.Size = new System.Drawing.Size(352, 45);
+            this.verzahltrack.TabIndex = 12;
+            this.verzahltrack.Value = 5;
+            this.verzahltrack.Scroll += new System.EventHandler(this.verzahltrack_Scroll);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(510, 375);
+            this.Controls.Add(this.verzahltrack);
+            this.Controls.Add(this.clear);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.entschlüsseltlbl);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.verzahltrack)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox entschlüsseltlbl;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Button clear;
+        private System.Windows.Forms.TrackBar verzahltrack;
+    }
+}
+
---
+++ verschlüsseln/verschlüsseln/Form1.cs
@@ -0,0 +1,113 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace verschlüsseln
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void verschlüsseln_Click(object sender, EventArgs e)
+        {
+           
+        }
+
+        private void entschlüsseld_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnentschl_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnentschl_Click_1(object sender, EventArgs e)
+        {
+
+        }
+
+        private void clear_Click(object sender, EventArgs e)
+        {
+            entschlüsseltlbl.Text = "";
+            label2.Text = "";
+        }
+
+        private void verzahltrack_Scroll(object sender, EventArgs e)
+        {
+            string eintext;      // zum verschlüsseln
+            int ver = 0;             // zum verschlüsseln
+            int verzahl = verzahltrack.Value;         // zum verschlüsseln
+
+
+            eintext = (entschlüsseltlbl.Text);
+
+            char[] CharEinText = eintext.ToCharArray();
+            label2.Text = "";
+            for (int i = 0; i < CharEinText.Length; i++)
+            {
+                ver = CharEinText[i];
+
+                if (verzahl > 26)   //Großzahlen größer 
+                {
+                    verzahl = verzahl % 26;
+                }
+                if (ver >= 97 & ver <= 122)         //KLeinbuchstaben bei entschlüsseln
+                {
+                    if (ver + verzahl > 122)
+                    {
+                        ver = ver + verzahl - 26;
+                    }
+
+                    else
+                    {
+                        ver = ver + verzahl;
+                    }
+                }
+
+
+                if (ver >= 65 & ver <= 90)           // Großbuchstaben bei entschlüsseln
+                {
+                    if (ver + verzahl > 90)
+                    {
+                        ver = ver + verzahl - 26;
+                    }
+
+                    else
+                    {
+                        ver = ver + verzahl;
+                    }
+                }
+
+                Convert.ToByte(ver);
+                string s = Encoding.ASCII.GetString(new byte[] { Convert.ToByte(ver) });
+                label2.Text += s;
+         
+           }
+        }
+       
+    }
+}
+
+
+    
+
